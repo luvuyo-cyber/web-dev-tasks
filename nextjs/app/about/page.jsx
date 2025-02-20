c0e262c540d1e1c32b5f7252fba39cb1e7323856
luvuyo-cyber <136644800+luvuyo-cyber@users.noreply.github.com>
2025-02-20 18:09:13 +0200

    Update metadata for About page and layout to reflect portfolio details

--- nextjs/app/about/page.jsx
+++ nextjs/app/about/page.jsx
@@ -1,3 +1,9 @@
+export const metadata = {
+  title: "About Luvuyo's Portfolio",
+  description: "Tech courses and Books",
+  keywords: "passive income, tech courses, tech books, tech tutorials",
+};
+
 const AboutPage = () => {
   return <div>About Page</div>;
 };
