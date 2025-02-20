c3537644e176091f25e2ee4d3b9430fc1eaf8216
luvuyo-cyber <136644800+luvuyo-cyber@users.noreply.github.com>
2025-02-20 17:32:13 +0200

    Add navigation links to HomePage and create About and Contact pages

--- nextjs/app/page.js
+++ nextjs/app/page.js
@@ -1,8 +1,21 @@
+import Link from "next/link";
+
 const HomePage = () => {
   return (
     <div>
-      Home Page
+      <h1>Home Page</h1>
       <button className="btn btn-primary">Button</button>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/about/contact">Contact</Link>
+        </li>
+      </ul>
     </div>
   );
 };
